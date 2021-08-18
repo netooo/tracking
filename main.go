41058f9d73c84c4e976872766c2ea0711bf0eff1
netooo <kazushi.mitsue@gmail.com>
2021-08-18 09:33:14 +0900

    Add: add task in main.go

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"github.com/urfave/cli"
+	"os"
+)
 
 func main() {
 	app := cli.NewApp()
@@ -8,4 +12,26 @@ func main() {
 	app.Usage = "Tracking CLI Client"
 	app.Version = "0.0.1"
 	app.EnableBashCompletion = true
+
+	projectIDFlag := &cli.IntFlag{
+		Name:  "project-id, P",
+		Usage: "project id",
+	}
+
+	app.Commands = []*cli.Command{
+		{
+			Name:    "add",
+			Aliases: []string{"a"},
+			Usage:   "Add task",
+			Action:  Add,
+			Flags: []cli.Flag{
+				projectIDFlag,
+			},
+			ArgsUsage: "<Task name>",
+		},
+	}
+	if err := app.Run(os.Args); err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, "Error:", err)
+		os.Exit(1)
+	}
 }
