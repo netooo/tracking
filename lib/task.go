784522fa3d81e1b82aeb7d5f55f2b1d4354d00df
netooo <kazushi.mitsue@gmail.com>
2021-09-11 23:20:12 +0900

    Add: add task

--- lib/task.go
+++ lib/task.go
@@ -1,7 +1,30 @@
 package tracking
 
+import (
+	"context"
+)
+
 type Task struct {
 	ID        int    `json:"id"`
 	Name      string `json:"name"`
 	ContentID int    `json:"content_id"`
+}
+
+func (t *Task) Add(ctx context.Context) error {
+	client, err := NewSheetClient(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := client.Append("Task", [][]interface{}{
+		{
+			t.ID,
+			t.Name,
+			t.ContentID,
+		},
+	}); err != nil {
+		panic(err)
+	}
+
+	return nil
 }
