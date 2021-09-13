727c0ea1905031de8a7105e7616f32c8c2423d22
netooo <kazushi.mitsue@gmail.com>
2021-09-13 23:54:41 +0900

    Add: random id of task

--- add_task.go
+++ add_task.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 	"github.com/netooo/TimeTracking/lib"
 	"github.com/urfave/cli"
+	"math/rand"
+	"time"
 )
 
 func AddTask(c *cli.Context) error {
@@ -14,6 +16,8 @@ func AddTask(c *cli.Context) error {
 		return errors.New("command failed")
 	}
 
+	rand.Seed(time.Now().UnixNano())
+	task.ID = rand.Intn(99999999) + 1
 	task.Name = c.String("name")
 	task.ContentID = c.Int("content")
 	if err := task.Add(context.Background()); err != nil {
