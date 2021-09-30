a957b47b2e8749c46ce49e1861c73f947e2a6262
netooo <kazushi.mitsue@gmail.com>
2021-09-30 09:53:23 +0900

    Add: start task

---
+++ start.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"errors"
+	"github.com/netooo/TimeTracking/lib"
+	"github.com/urfave/cli"
+	"time"
+)
+
+func Start(c *cli.Context) error {
+
+	taskID := c.Int("task")
+	if taskID == 0 {
+		return errors.New("command failed")
+	}
+
+	taskList, err := tracking.TaskRead()
+	if err != nil {
+		return err
+	}
+
+	ok := false
+	task := tracking.Task{}
+	for _, t := range taskList {
+		if taskID == t.ID {
+			ok = true
+			task = *t
+			break
+		}
+	}
+	if !ok {
+		return errors.New("task not found")
+	}
+
+	histories, err := tracking.TrackRead()
+	if err != nil {
+		return err
+	}
+
+	if len(histories) > 0 {
+		lastLog := histories[len(histories)-1]
+		if lastLog.FinishedAt.IsZero() {
+			return errors.New("other task is running")
+		}
+	}
+
+	track := tracking.Track{
+		TaskID:    task.ID,
+		TaskName:  task.Name,
+		StartedAt: time.Now(),
+	}
+
+	if err := track.Start(); err != nil {
+		return err
+	}
+
+	return nil
+}
