f7fbca2399378f31e4396f4d58759403a7e7e472
netooo <kazushi.mitsue@gmail.com>
2021-10-24 23:34:32 +0900

    Modify: var name

--- zsh_prompt.sh
+++ zsh_prompt.sh
@@ -2,10 +2,10 @@
 
 tracking_current() {
   local current=$(tracking c)
-  local current_time=$(echo $current | grep Duration | cut -d ' ' -f 2)
-  if [ ! -n "$currrent_time" ]; then
+  local duration=$(echo $current | grep Duration | cut -d ' ' -f 2)
+  if [ ! -n "$duration" ]; then
       echo "NotTracking"
   else
-      echo "[$current_time]"
+      echo "[$duration]"
   fi
 }
